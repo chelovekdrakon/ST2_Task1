4071fc1107d965b5661d13c33ed2099004d11244
Morau Fiodar <fiodar.morau@sharekey.com>
2019-06-01 18:40:54 +0300

    Task2: Single Page App init

---
+++ image gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  image gallery
+//
+//  Created by Фёдор Морев on 6/1/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ image gallery/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  image gallery
+//
+//  Created by Фёдор Морев on 6/1/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
