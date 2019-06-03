8db35f27cf00c2132753560434ce87180fbea988
Morau Fiodar <fiodar.morau@sharekey.com>
2019-06-03 09:44:33 +0300

    refactoring: removed .storyboard. UILoader didMoveToSuperview didnt have super. FirstScreenViewController code-style

--- image gallery/components/UILoader.m
+++ image gallery/components/UILoader.m
@@ -24,6 +24,7 @@
 }
 
 - (void)didMoveToSuperview {
+    [super didMoveToSuperview];
     self.handler();
 }
 
--- image gallery/view-controllers/FirstScreenViewController.m
+++ image gallery/view-controllers/FirstScreenViewController.m
@@ -20,19 +20,20 @@
     
     self.title = @"Image Gallery";
     
-    UIBarButtonItem *barButtonAdd = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(handleAddPress:)];
+    UIBarButtonItem *barButtonAdd = [[UIBarButtonItem alloc]
+                                     initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                     target:self
+                                     action:@selector(handleAddPress:)];
     barButtonAdd.tintColor = [UIColor redColor];
     self.navigationItem.rightBarButtonItem = barButtonAdd;
     
-    CGSize screenSize = [UIScreen mainScreen].bounds.size;
+//    CGSize screenSize = [UIScreen mainScreen].bounds.size;
     CGSize controllerSize = self.view.frame.size;
     
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, screenSize.width, screenSize.height)];
-    scrollView.contentSize = screenSize;
-    
-//    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(10, 10, 10, 1000)];
-//    view.backgroundColor = [UIColor yellowColor];
-//    [scrollView addSubview:view];
+    UIScrollView *scrollView = [[UIScrollView alloc]
+                                initWithFrame:CGRectMake(0, 0, controllerSize.width, controllerSize.height)];
+    scrollView.contentSize = controllerSize;
+    scrollView.userInteractionEnabled = YES;
     
     [self.view addSubview:scrollView];
     _mainScrollView = scrollView;
@@ -43,10 +44,6 @@
 - (void)handleAddPress:(id)sender {
     SecondScreenViewController *vc = [[SecondScreenViewController alloc] init];
     vc.view.backgroundColor = [UIColor whiteColor];
-    
-//    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
-//    DetailViewController *detailViewController = [storyboard instantiateViewControllerWithIdentifier:@"DetailViewController"];
-//    [self.navigationController pushViewController:detailViewController animated:YES];
     
     [self.navigationController pushViewController:vc animated:YES];
 }
