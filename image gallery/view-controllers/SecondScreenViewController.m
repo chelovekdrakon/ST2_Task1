f8bcf717b89c7de5f3ab0fc6a887ae4d8691bc28
Morau Fiodar <fiodar.morau@sharekey.com>
2019-06-05 00:33:08 +0300

    refactoring: SecondScreen imaged amount 1 -> 30

--- image gallery/view-controllers/SecondScreenViewController.m
+++ image gallery/view-controllers/SecondScreenViewController.m
@@ -32,7 +32,7 @@ typedef void (^completionHandler)(NSData *data, NSURLResponse *response, NSError
     [super viewDidLoad];
     
     _imageHeight = 100;
-    _imagesAmount = 1;
+    _imagesAmount = 30;
     self.navigationItem.title = @"Select Item";
     
     UIBarButtonItem *barButtonClose = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(handleClosePress:)];
